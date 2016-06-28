b99bc370b85985ac1497954185ec3fbc8ec18655
Sunil <mynameis.sunilk@gmail.com>
2016-06-28 09:17:43 -0400

    forgot getters/setters

--- src/Song.java
+++ src/Song.java
@@ -13,4 +13,25 @@ class Song {
     public void play(){
         System.out.println("Song: " + mSongName + " Artist: " + mAlbumName +" Album: " + mAlbumName);
     }
+
+    public void setSongName(String name){
+        mSongName = name;
+    }
+
+    public String getSongName(){
+        return mSongName;
+    }
+    public void setArtistName(String name){
+        mArtistName = name;
+    }
+    public String getArtistName(){
+        return mArtistName;
+    }
+
+    public void setAlbumName(String name){
+        mAlbumName = name;
+    }
+    public String getmAlbumName(){
+        return mAlbumName;
+    }
 }
